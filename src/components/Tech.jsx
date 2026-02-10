6b359892d7ba5b5c0099724cd3362eb76c3bbf1a
SHAILENDRA <shailendrakumar9519842594@gmail.com>
2026-02-10 11:44:16 +0530

    n

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,7 +5,7 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
-const Tech = () => {
+const Tech = ({ theme }) => {
     return (
         <div className='flex flex-row flex-wrap justify-center gap-10'>
             {technologies.map((technology) => (
